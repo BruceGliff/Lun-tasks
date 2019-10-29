aad4fa263e653e7924a1c2ef5e500c1056d955fd
BruceGliff <ilyaawaA@yandex.ru>
2019-10-29 16:03:45 +0300

    update

--- Star1/FD/main.c
+++ Star1/FD/main.c
@@ -3,16 +3,19 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int compare(int fd1, int fd2);
 
 int main()
 {
-    int fd1 = open("xx", O_WRONLY | O_CREAT);
-    int fd2 = open("xx", O_RDONLY);
+    int fd1 = open("1", O_WRONLY);
+    sleep(20);
+    int fd2 = open("1", O_RDONLY);
 
-    assert(fd1 >= 0 && fd2 >= 0);
-    printf("%d", compare(fd1, fd2));
+    assert(fd1 >= 0);assert(fd2 >= 0);
+    printf("%d\n", compare(fd1, fd2));
 
     return 0;
 }
@@ -21,22 +24,31 @@ int compare(int fd1, int fd2)
 {
     struct stat stat1;
     struct stat stat2;
+    struct stat stat1_;
 
-    int status = fstat(fd1, &stat1);
+    int status = 0;
+
+    again:
+    status = fstat(fd1, &stat1);
     if (status == -1)
         return -1;
 
     status = fstat(fd2, &stat2);
     if (status == -1)
         return -1;
+    
+    status = fstat(fd1, &stat1_);
+    if (status == -1)
+        return -1;
 
-    // inode unique for each file in one file system
+    if (stat1_.st_ino != stat1.st_ino || stat1_.st_rdev != stat1.st_rdev)
+        goto again;
 
-    // devices must be indentical for one file system
+    if (stat2.st_ino != stat1.st_ino || stat2.st_rdev != stat1.st_rdev)
+        return 0;
 
-    if ((stat2.st_ino == stat1.st_ino) && (stat2.st_dev == stat1.st_dev))
-        return 1;
+    
 
-    return 0;
+    return 1;
 
 }
