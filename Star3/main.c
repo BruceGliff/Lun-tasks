f51c537a5315d95f7c09d8731295695f7b441e6a
BruceGliff <ilyaawaA@yandex.ru>
2019-11-04 23:45:56 +0300

    no processors lost

--- Star3/main.c
+++ Star3/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/prctl.h>
 
 pid_t reader_pid;
 pid_t writer_pid;
@@ -11,6 +12,7 @@ int bit;
 char tmp;
 
 int flag = 1;
+int ChFlag = 1;
 
 FILE * openFile(int argc, char * argv[]);
 void voidHandler(int s){flag = 0;}
@@ -47,6 +49,10 @@ void ChildHandler(int sig)
 {
 	exit(1);
 }
+void ChildExit(int sig)
+{
+	exit(1);
+}
 
 int main(int argc, char **argv)
 {
@@ -56,18 +62,26 @@ int main(int argc, char **argv)
 
     reader_pid = getpid();
 
-    // prepare to child death
+    // prepare to child's death
     sigset_t ChildDeath;
     signal(SIGCHLD, &ChildHandler);
     sigemptyset(&ChildDeath);
     sigaddset(&ChildDeath, SIGCHLD); 
-    
+	
     // wait part
     sigset_t Wait;
     int sig;
     signal(SIGURG, &voidHandler);
     sigemptyset(&Wait);
     sigaddset(&Wait, SIGURG);
+    
+    // prepare for parent's death
+    sigset_t ChExit;
+    int SigExit;
+    signal(SIGHUP, &ChildExit);
+    sigemptyset(&ChExit);
+    sigaddset(&ChExit, SIGHUP);
+
 
     pid_t fork_pid = fork();
     if (fork_pid < 0) {
@@ -76,6 +90,7 @@ int main(int argc, char **argv)
 
     if (fork_pid == 0) {
         // writer
+	prctl(PR_SET_PDEATHSIG, SIGHUP);
 	writer_pid = getpid();
 
         // prepare part
@@ -93,7 +108,7 @@ int main(int argc, char **argv)
 
         // sync part
         close(pipefd[0]);
-        write(pipefd[1], "", 1); // процесс готов к приёму сигнала
+        write(pipefd[1], "", 1); // process is ready
         close(pipefd[1]);
 
 	// wait part
@@ -104,6 +119,7 @@ int main(int argc, char **argv)
     if (fork_pid > 0) 
     {
         // reader
+
 	writer_pid = fork_pid;
 	FILE * file = openFile(argc, argv);
 	if (!file)
@@ -119,7 +135,7 @@ int main(int argc, char **argv)
 	// sync part
         char c;
         close(pipefd[1]);
-        read(pipefd[0], &c, 1); // ожидание готовности потомка
+        read(pipefd[0], &c, 1); // process is wait
         close(pipefd[0]);
 	// send part
 	while(1)
