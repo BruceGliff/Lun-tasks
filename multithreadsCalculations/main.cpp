5f00479207df3beb027796cefa58877f4c2c3604
BruceGliff <ilyaawaA@yandex.ru>
2020-03-06 13:59:26 +0300

    work version

--- multithreadsCalculations/main.cpp
+++ multithreadsCalculations/main.cpp
@@ -20,99 +20,103 @@ struct cacheLine
     double out;
     Condition status;
 };
-void integrate(double begin, double end, char * cache);
+void integrate(double begin, double end, char * cache) noexcept;
 struct MyThread
 {
     // Task part
     double begin = -10e3;
     double end = 10e3;
-    int maxThreads;
-    double del;
+    double del = 0.f;
 
 
     char * allCache_ = nullptr;
     std::thread * queue_ = nullptr;
     int cacheSize = 128;
-    int deltaCache = 32;
     int threadsCount_ = 0;
-
+    int maxThreads = 0;
+    int currentThreads = 0;
 
     MyThread (int threadsCount) : threadsCount_(threadsCount), maxThreads(std::thread::hardware_concurrency())
     {
         del = (end - begin) / threadsCount_;
         allCache_ = new char[cacheSize * threadsCount_];
+        std::fill (allCache_, allCache_ + cacheSize * threadsCount_, 0);
 
         if (threadsCount_ > 1)
         {
-            try
-            {
-                queue_ = (std::thread *) ::operator new (sizeof(std::thread) * (threadsCount_ - 1));       
-            }
-            catch(std::bad_alloc const & e)
+            queue_ = (std::thread *) ::operator new (sizeof(std::thread) * (threadsCount_ - 1), std::nothrow);
+            if (!queue_)      
             {
                 delete[] allCache_;
-                std::cerr << e.what() << '\n';
-                throw;
+                throw std::bad_alloc{};
             }
         }
     }
 
     void Launch()
     {
-        try{
         for(int i = 0; i != threadsCount_ - 1; ++i)
-        {        
+        {  
+            ++currentThreads;     
             new (&queue_[i]) std::thread    (std::thread{integrate, 
+                                            begin + i * del,
                                             begin + (i + 1) * del,
-                                            begin + (i + 2) * del,
-                                            allCache_ + (i + 1) * cacheSize
+                                            allCache_ + i * cacheSize + deltaCache
                                             });
-
-            queue_[i].detach();
+            queue_[i].detach(); 
         }
-        }
-        catch (std::bad_alloc const & e)
-        { std::cerr << e.what() << '\n'; }
     }
     void LaunchMain() const noexcept
     {
-        integrate(begin, begin + del, allCache_);
+        integrate   (begin + (threadsCount_ - 1) * del, 
+                    begin + threadsCount_ * del, 
+                    allCache_ + (threadsCount_ - 1) * cacheSize + deltaCache);
     }
 
-    double Count()
+    double Count() noexcept
     {
         double result = 0;
-        bool isFinished = false;
-        while(!isFinished)
+        while (!areThreadsDone()) {}
+
+        for (int i = 0; i != threadsCount_; ++i)
         {
-            isFinished = true;
-            for (int i = 0; i != threadsCount_; ++i)
-            {
-                cacheLine * out = reinterpret_cast<cacheLine *>(allCache_ + i * cacheSize + deltaCache);
-                if (out->status == Condition::finished)
-                {
-                    result += out->out;
-                    out->status = Condition::checked;
-                }
-                else if(out->status != Condition::checked)
-                    isFinished = false;
-            } 
-        }
+            cacheLine * out = reinterpret_cast<cacheLine *>(allCache_ + i * cacheSize + deltaCache);
+            result += out->out;
+        } 
+
         return result;
+    }
+
+    bool areThreadsDone()
+    {
+        for (int i = 0; i != currentThreads; ++i)
+        {
+            cacheLine * out = reinterpret_cast<cacheLine *>(allCache_ + i * cacheSize + deltaCache);
+            if (out->status == Condition::finished)
+                out->status == Condition::checked;
+            else if(out->status != Condition::checked)
+                return false;            
+        }
+        return true;
     }
 
     ~MyThread()
     {
+        while(!areThreadsDone()) {}
+
+        for (int i = 0; i != currentThreads; ++i)
+            queue_[i].~thread();
+
         delete[] allCache_;
         if (threadsCount_ > 1)
             delete queue_;
     }
 };
 
-void integrate(double begin, double end, char * cache)
+void integrate(double begin, double end, char * cache) noexcept
 {
     double const desc = 10e-6;//-6
-    cacheLine * out = reinterpret_cast<cacheLine *>(cache + deltaCache);
+    cacheLine * out = reinterpret_cast<cacheLine *>(cache);
     out->status = Condition::inWork;
     double sum = 0;
     while (begin <= end)
@@ -132,6 +136,8 @@ int main(int argc, char * argv[])
         threadsCount = 1;
     else
         threadsCount = strtol(argv[1], NULL, 10);
+    if (!threadsCount)
+        return 0;
 
     MyThread * Thread;
    
@@ -141,12 +147,10 @@ int main(int argc, char * argv[])
     catch (std::bad_alloc const & e)
     { std::cerr << "Bad alloc: \n" << e.what(); return 0; }
 
-    try
-    {
-        Thread->Launch();
-    }
-    catch (std::bad_alloc const & e)
-    { std::cerr << "Bad alloc: \n" << e.what(); return 0;}
+    try{
+    Thread->Launch();
+    } catch (std::system_error const & e)
+    { std::cerr << "System error: \n" << e.what(); delete Thread; return 0; }
 
     Thread->LaunchMain();
 
