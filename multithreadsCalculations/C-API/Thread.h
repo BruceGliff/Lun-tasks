67d9d38308be7c78f174d342ac03b04fbfbccfc9
BruceGliff <ilyaawaA@yandex.ru>
2020-03-30 20:41:15 +0000

    some comments add

--- multithreadsCalculations/C-API/Thread.h
+++ multithreadsCalculations/C-API/Thread.h
@@ -3,21 +3,20 @@
 
 struct Threads : public Task
 {
+    // memory for threads
     pthread_t * threads = nullptr;
+    // general info 
     CPU_info const info{};
     
-    Threads(int threadsCount) : Task{threadsCount}, threads( (pthread_t *) calloc (threadsCount_, sizeof(pthread_t)) )
-    {
-        if(!info.isPropreateAlgorithm)
-        {
-            std::cerr << "Unrecognized CPU's configuration, pls send cpuinfo and core_cpu_list files" << std::endl;
-            exit(-1);
-        }
-    }
+    // Create Task discription and memory for threads
+    Threads(int threadsCount) : Task{threadsCount}, threads( (pthread_t *) calloc (threadsCount_, sizeof(pthread_t)) ) {}
     ~Threads() { free(threads); }
 
+    // Start calculating task
     double launch()
     {
+        //--------------------------
+        //  init pthread parametrs
         pthread_attr_t attr;
         int s = 0;
         s = pthread_attr_init(&attr);
@@ -27,23 +26,31 @@ struct Threads : public Task
             exit(-1);
         }
 
-        for (int i = 0; i !=  threadsCount_; ++i)
+
+        //--------------------------
+        //  chose propreate algorithm
+        switch (info.algorithm)
         {
-            s = set_stick_attr(i % info.ready_cpus, &attr);
-            if (s)
+        case ECPU_Alg::Rise_Order:
+            for (int i = 0; i !=  threadsCount_; ++i)
             {
-                std::cerr << "Cann't stick thread to current core" << std::endl;
-                exit(-1);
+                set_stick_attr(i % info.ready_cpus, &attr);
+                threadCreate(i, attr);
             }
-
-            s = pthread_create(&threads[i], &attr, Task::integrate, GetTask(i));
-            if (s)
-            {
-                std::cerr << "Cann't create thread" << std::endl;
-                exit(-1);
-            }
+            break;
+        case ECPU_Alg::Default:
+            for (int i = 0; i !=  threadsCount_; ++i)
+                threadCreate(i, attr);
+            break;
+        
+        default:
+            std::cerr << "Unexpected program behavior!" << std::endl;
+            exit(-1);
+            break;
         }
-
+        
+        //--------------------------
+        //   waiting till calculations are done
         double res = 0;
         for (int i = 0; i != threadsCount_; ++i)
         {   
@@ -61,12 +68,35 @@ struct Threads : public Task
         return res;
     }
 
+
+    // Creating thread with param in attr
+    int threadCreate(int indx, pthread_attr_t const & attr)
+    {
+        int const s = pthread_create(&threads[indx], &attr, Task::integrate, GetTask(indx));
+        if (s)
+        {
+            std::cerr << "Cann't create thread" << std::endl;
+            exit(-1);
+        }
+
+        return 0;
+    }
+
+
+    // Stick thread to specific core
     int set_stick_attr(int core_id, pthread_attr_t * attr) 
     {
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
         CPU_SET(core_id, &cpuset);
   
-        return pthread_attr_setaffinity_np(attr, sizeof(cpu_set_t), &cpuset);
+        int s = pthread_attr_setaffinity_np(attr, sizeof(cpu_set_t), &cpuset);
+        if (s)
+        {
+            std::cerr << "Cann't stick thread to current core" << std::endl;
+            exit(-1);
+        }
+        
+        return 0;
     }
 };
--- multithreadsCalculations/C-API/cpu_optimization.h
+++ multithreadsCalculations/C-API/cpu_optimization.h
@@ -6,12 +6,19 @@
 
 // For different algorithms PART
 //___________________________________
+
+// enum for different algorithms
+enum class ECPU_Alg
+{
+    Default,            // Unrecognized algorithm
+    Rise_Order          // In N-cores CPU 0 and N processor on the one core.
+};
+
 struct Condition
 {
     int core_id = 0;
     bool isBusy = false;
 };
-
 struct Pair
 {
     Condition first;
@@ -22,15 +29,15 @@ struct Pair
 struct CPU_info
 {
     int const ready_cpus;
-    bool const isPropreateAlgorithm;
+    ECPU_Alg const algorithm;
 
-    CPU_info() : ready_cpus(std::thread::hardware_concurrency()), isPropreateAlgorithm(GetCpuConfiguration()) {}
+    CPU_info() : ready_cpus(std::thread::hardware_concurrency()), algorithm(GetCpuConfiguration()) {}
 
 
-    bool GetCpuConfiguration();
+    ECPU_Alg GetCpuConfiguration();
 };
 
-bool CPU_info::GetCpuConfiguration()
+ECPU_Alg CPU_info::GetCpuConfiguration()
 {
     char const * cpuifo = "/sys/bus/cpu/devices/cpu0/topology/core_cpus_list";
 
@@ -38,7 +45,7 @@ bool CPU_info::GetCpuConfiguration()
     if (!cpuinfo.is_open())
     {
         std::cerr << "Cann't open " << cpuifo << std::endl;
-        return false;
+        return ECPU_Alg::Default;
     }
 
     int first = 0;
@@ -47,5 +54,9 @@ bool CPU_info::GetCpuConfiguration()
     cpuinfo >> first >> trash >> second;
 
     cpuinfo.close();
-    return (second - first) == (ready_cpus / 2);
+    if ((second - first) == (ready_cpus / 2))
+        return ECPU_Alg::Rise_Order;
+
+    std::cerr << "Unrecognized cpu's algorithm!" << std::endl;
+    return ECPU_Alg::Default;
 }
