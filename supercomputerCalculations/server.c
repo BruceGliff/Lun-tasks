88c9a0db77da655c5a5817eba7cb7db065f244c4
BruceGliff <ilyaawaA@yandex.ru>
2020-05-06 12:25:43 +0000

    add supercomputer calculations

---
+++ supercomputerCalculations/server.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string.h>
+#include <unistd.h>
+
+void castToWorker(struct sockaddr_in a);
+struct sockaddr_in rcvFromWorker();
+
+void establichConnection(struct sockaddr_in);
+
+int main()
+{
+    establichConnection(rcvFromWorker());
+
+
+
+    puts("before sleep");
+    sleep(1);
+    puts("after sleep");
+}
+
+void establichConnection(struct sockaddr_in workerAddr)
+{
+    int sk = socket(AF_INET, SOCK_STREAM, 0);
+    if (sk < 0) 
+        perror("ERROR opening socket");
+
+    if (connect(sk, (struct sockaddr *) &workerAddr, sizeof(workerAddr)) < 0) 
+        perror("ERROR connecting");
+
+    int n = write(sk, "EC", 2);
+    if (n < 0) 
+        perror("ERROR writing to socket");
+
+    puts("end establish connection");
+    sleep(1);
+}
+
+void castToWorker(struct sockaddr_in workaddr)
+{
+    puts("CastBegin");
+
+    int bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bcast_sock < 0)
+        perror("cannot create sock for broadcast\n");
+
+    int broadcastEnable = 1;
+    //int ret = setsockopt(bcast_sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+    //if (ret < 0)    
+        //perror("cannot set up socket for broadcast\n");
+
+    char mess[] = "TOWORKER";
+    if(sendto(bcast_sock, mess, strlen(mess), 0, (struct sockaddr *) &workaddr, sizeof(workaddr) < 0))
+        perror("sendto");
+
+    close(bcast_sock);
+    puts("CastEnd");
+
+    return;
+}
+
+struct sockaddr_in rcvFromWorker()
+{
+    puts("Rcv from worker begin");
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in rec_addr; int recaddrlen = sizeof(rec_addr);
+    int port = 101;
+    int bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bcast_sock < 0)
+        perror("cannot create sock rcvFromServer\n");
+    int broadcastEnable=1;
+    if(setsockopt(bcast_sock,SOL_SOCKET,SO_BROADCAST,&broadcastEnable,sizeof(int)) == -1)
+    {
+            perror("setsockopt");
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_BROADCAST; // ANY
+    serv_addr.sin_port = htons(port);
+
+    if(bind(bcast_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr))==-1)
+    {
+            close(bcast_sock);
+            perror("cannot bind");
+    }
+
+    char recvString[100];
+    memset(&rec_addr, 0, recaddrlen);
+    int res = recvfrom(bcast_sock, recvString, 99, 0, (struct sockaddr *) &rec_addr, &recaddrlen);
+    if (res < 0)
+    {
+        perror("cannot recieve");
+    }
+
+    recvString[res] = '\0';
+    printf("Accept: %s\n", recvString);
+
+    if(close(bcast_sock) == -1)
+        perror("CLOSE err in rcv");
+
+    printf("Datagram size: %d.\n", res);
+    //printf("Datagram's IP address is: %s\n", inet_ntoa(rec_addr.sin_addr));
+    printf("Datagram's port is: %d\n", (int) ntohs(rec_addr.sin_port));
+
+    puts("Rcv from serv end");
+    return rec_addr;
+}
---
+++ supercomputerCalculations/worker.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h> 
+
+void broadcastToServer();
+struct sockaddr rcvFromServer();
+void establishConnection();
+
+int main()
+{
+    broadcastToServer();
+    establishConnection();
+
+
+    puts("before sleep");
+    sleep(1);
+    puts("after sleep");
+
+    
+    
+}
+void establishConnection()
+{
+    int sk = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serv_addr, cli_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(101);
+
+    if (bind(sk, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        perror("ERROR on binding");
+
+    listen(sk, 5);
+
+    int clilen = sizeof(cli_addr);
+    int newsockfd = accept(sk, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) 
+        perror("ERROR on accept");
+
+    puts("finish establ connectio");
+}
+
+void broadcastToServer()
+{
+    puts("BroadCastBegin");
+
+
+    struct sockaddr_in ToServer;
+    int port = 101;
+    int bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bcast_sock < 0)
+        perror("cannot create sock for broadcast\n");
+
+    int broadcastEnable = 1;
+    int ret = setsockopt(bcast_sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+    if (ret < 0)    
+        perror("cannot set up socket for broadcast\n");
+
+
+    ToServer.sin_family = AF_INET;
+    ToServer.sin_addr.s_addr = INADDR_BROADCAST; // ANY
+    ToServer.sin_port = htons(port);
+
+    char const * mess = "TOSERVER";
+
+    if(sendto(bcast_sock, mess, strlen(mess), 0, (struct sockaddr *)&ToServer, sizeof(struct sockaddr_in)) < 0)
+        perror("sendto");
+
+    if(close(bcast_sock))
+        perror("close worker");
+
+    puts("BroadcastEnd");
+
+    return;
+}
+
+struct sockaddr rcvFromServer()
+{
+    puts("Rcv from serv begin");
+    struct sockaddr_in serv_addr;
+    struct sockaddr rec_addr; int recaddrlen = sizeof(rec_addr);
+    int port = 101;
+    int bcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bcast_sock < 0)
+        perror("cannot create sock rcvFromServer\n");
+    int broadcastEnable=1;
+    if(setsockopt(bcast_sock,SOL_SOCKET,SO_BROADCAST,&broadcastEnable,sizeof(int)) == -1)
+    {
+            perror("setsockopt");
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_BROADCAST; // ANY
+    serv_addr.sin_port = htons(port);
+
+    if(bind(bcast_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr))==-1)
+    {
+            close(bcast_sock);
+            perror("cannot bind");
+    }
+
+    char recvString[100];
+    memset(&rec_addr, 0, recaddrlen);
+    int res = recvfrom(bcast_sock, recvString, 99, 0, &rec_addr, &recaddrlen);
+    if (res < 0)
+    {
+        perror("cannot recieve");
+    }
+
+    recvString[res] = '\0';
+    printf("Accept: %s\n", recvString);
+
+    if(close(bcast_sock) == -1)
+        perror("CLOSE err in rcv");
+
+
+    puts("Rcv from serv end");
+    return rec_addr;
+}
