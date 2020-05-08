166d09720d8477e507bdf56df6e635b28979c1f5
BruceGliff <ilyaawaA@yandex.ru>
2020-05-08 23:39:57 +0000

    no busy wait

--- supercomputerCalculations/serverMain.cc
+++ supercomputerCalculations/serverMain.cc
@@ -19,10 +19,20 @@
     }
 
 int establishConnection(int port);
+void * acceptc (void * data);
+int free_indx = 0;
+pthread_mutex_t mutex;
+int listen_sk = 0;
 
+struct pointery
+{
+    TasksQueue * q;
+    pthread_t * t;
+};
 
 int main()
 {
+    pthread_mutex_init(&mutex, NULL);
     //create pipe for structures and pthread
     int const size_addr = sizeof(sockaddr_in);
 
@@ -34,54 +44,64 @@ int main()
     sockaddr_in worker_addr;
     int port = 102;
 
-    pthread_t * th_queue[256];
-    int free_indx = 0;
+    pthread_t th_queue[256];
+
 
     int pipe_fd[2];
     if (pipe(pipe_fd) == -1)
         ERROR("Err create pipe");
-    if(fcntl(pipe_fd[0], F_SETFL, O_NONBLOCK))
-        ERROR("Err set pipe nonblock");
     
     TasksQueue q(pipe_fd[1]);
 
-    int listen_sk = establishConnection(100);
+    listen_sk = establishConnection(100);
     listen(listen_sk, 256);
 
     double res = 0;
-    while(1)
-    { 
-        memset(&worker_addr, 0, size_addr);
-        socklen_t a=0;
-        
-        int reslen = read(pipe_fd[0], &res, sizeof(double));
-        if (reslen == sizeof(double))
-            break;
-        int newSocket = accept(listen_sk, (struct sockaddr *) &worker_addr, &a);
-        if (newSocket == -1)
-            continue;
-        
-        th_queue[free_indx] = (pthread_t *) malloc (sizeof(pthread_t));
-        ConnectionSettings set = {&q, newSocket};
-        if (pthread_create(th_queue[free_indx++], NULL, TaskSender, &set) != 0)
-            ERROR("Err create thread");
-    }
+    pointery p = {&q, th_queue};       
 
+        pthread_t acc;
+        pthread_create(&acc, NULL, acceptc, &p);
+
+    int reslen = read(pipe_fd[0], &res, sizeof(double));
+
+    pthread_mutex_lock(&mutex);
+    if (shutdown(listen_sk, SHUT_RDWR) == -1);
+        perror(0);
     close(listen_sk);
     close(pipe_fd[0]);
+    pthread_cancel(acc);
     for (int i = 0; i != free_indx; ++i)
-        pthread_join(*(th_queue[i]), NULL);
+        pthread_join(th_queue[i], NULL);
 
     printf("RESULT: %f\n", res);
+
+    pthread_mutex_unlock(&mutex);
 
     return 0;
 }
 
 
+void * acceptc (void * data)
+{
+    pointery * p = (pointery *) data;
+    while(1)
+    {       
+        int newSocket = accept(listen_sk, NULL, NULL);
+        if (newSocket == -1)
+            return NULL;
+        pthread_mutex_lock(&mutex);
+        ConnectionSettings set = {p->q, newSocket};
+        if (pthread_create(&(p->t[free_indx++]), NULL, TaskSender, &set) != 0)
+            ERROR("Err create thread");
+
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
 
 int establishConnection(int port)
 {
-    int sk = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    int sk = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
